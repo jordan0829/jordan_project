8f4a8b1f1d8abe6133f186c4725fe9452ef0d046
jordan0829 <tatsuyachiba0829@gmail.com>
2024-12-29 23:09:53 +0900

    TODOメッセージ定数を追加

---
+++ src/client/constants/messages.ts
@@ -0,0 +1,17 @@
+export const MESSAGES = {
+    TODO: {
+        COMPLETE_TODOS: {
+            TITLE: "完了のTODO",
+            BACK_BUTTON: "戻す",
+        },
+        INCOMPLETE_TODOS: {
+            TITLE: "未完了のTODO",
+            DELETE_BUTTON: "削除",
+            COMPLETE_BUTTON: "完了",
+        },
+        INPUT_TODO: {
+            ADD_BUTTON: "追加",
+            TODO_LIMIT_MESSAGGE: "登録できるTODOは5つまでです。",
+        },
+    },
+} as const;
